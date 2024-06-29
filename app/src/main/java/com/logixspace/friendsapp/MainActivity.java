95503a115978a9dd8fa10ae58ee4b37dfac1bfa2
Judit-MJ <juditmerinajohny@gmail.com>
2024-06-29 15:14:02 +0530

    msgg

--- app/src/main/java/com/logixspace/friendsapp/MainActivity.java
+++ app/src/main/java/com/logixspace/friendsapp/MainActivity.java
@@ -12,10 +12,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
         EditText ed1,ed2,ed3,ed4;
         AppCompatButton b1;
+        String apiurl="https://friendsapi-re5a.onrender.com/adddata";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +45,40 @@ public class MainActivity extends AppCompatActivity {
                 String getPassword=ed2.getText().toString();
                 String getFriend=ed3.getText().toString();
                 String getDescrip=ed4.getText().toString();
-                Toast.makeText(getApplicationContext(),getUsername+" "+getPassword+" "+getFriend+" "+getDescrip,Toast.LENGTH_LONG).show();
+
+//creating json obj
+                JSONObject friend=new JSONObject();
+                try {
+                    friend.put("name",getUsername);
+                    friend.put("friendName",getPassword);
+                    friend.put("friendNickName",getFriend);
+                    friend.put("DescribeYourFriend",getDescrip);
+                }
+                catch(JSONException e){
+                    throw new RuntimeException(e);
+                }
+                //json obj req creation
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiurl,
+                        friend,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "Added Successfully", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "Something Went Wrong", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                );
+                //Request Queue
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
             }
         });
 
